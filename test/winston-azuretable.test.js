0cf2b79766a4c472d305a522f8bd38695d65d484
Jason <github@jasonpoon.ca>
2014-10-06 20:22:06 -0700

    * Upgrade to Azure 0.10.0 * Test against azure storage emulator

--- test/winston-azuretable.test.js
+++ test/winston-azuretable.test.js
@@ -6,6 +6,18 @@ var winston = require('winston');
 var azureLogger = require("../lib/winston-azuretable.js").AzureLogger;
 
 describe('azure logger', function() {
+    var _tableService = azure.createTableService('UseDevelopmentStorage=true');
+
+    beforeEach(function() {
+        _tableService.listTablesSegmented(null, function(error, result, response) {
+            if (result.entities) {
+                result.entities.foreach(function(tableName) {
+                    tableService.deleteTableIfExists(tableName);
+                })
+            }
+        })
+    })
+
     describe('ctor', function() {
         it('storage name required', function() {
             expect(function() {
@@ -19,6 +31,33 @@ describe('azure logger', function() {
                     account: 'some account'
                 })
             }).to.throw('azure storage account key required.');
+        });
+
+        it('happy path', function(done) {
+            var logger = new winston.transports.AzureLogger({
+                useDevStorage: true,
+                callback: function() { 
+                    _tableService.listTablesSegmented(null, function(err, result, response) {
+                        expect(result.entries).to.have.length('1');
+                        expect(result.entries).to.include('log');
+                    })
+
+                    done(); 
+                }
+            });
+
+        });
+    });
+
+    describe('log', function() {
+        it('happy path', function(done) {
+            var logger = new winston.transports.AzureLogger({
+                useDevStorage: true,
+                callback: function() { done(); }
+            });
+
+            var msg = Math.random().toString(36).replace(/[^a-z]+/g, '');
+            logger.log('level', msg, '', function() { done() });
         });
     });
 });
