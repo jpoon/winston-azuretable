42985b91775c886431534b0572b1644ce2319c05
Jason <github@jasonpoon.ca>
2014-10-11 23:19:21 -0700

    Implement query function and UTs

--- test/winston-azuretable.test.js
+++ test/winston-azuretable.test.js
@@ -7,23 +7,23 @@ var expect = require('chai').expect,
 describe('azure logger', function() {
     var _tableService = azure.createTableService('UseDevelopmentStorage=true');
 
-    beforeEach(function(done) {
+    afterEach(function(done) {
         _tableService.listTablesSegmented(null, function(error, result, response) {
-            if (result.entries) {
+            expect(error).to.be.null;
 
-                var handleError = function(error) { 
-                    if (error) {
-                        console.log('Deleting Table Error: ' + error);
-                    }
+            if (result.entries && result.entries.length > 0) {
+                var deleteTableCallback = function(error) { 
+                    expect(error).to.be.null;
+                    done();
                 };
 
                 for (var index = 0; index < result.entries.length; index++) {
                     var tableName = result.entries[index];
-                    _tableService.deleteTableIfExists(tableName, handleError);
+                    _tableService.deleteTableIfExists(tableName, deleteTableCallback);
                 }
+            } else {
+                done();
             }
-
-            done();
         });
     });
 
@@ -74,7 +74,9 @@ describe('azure logger', function() {
                 partitionKey: expectedPartitionKey,
                 callback: function() {
 
-                    logger.log(expectedLevel, expectedMsg, { propName1: 'propValue1', propName2: 'propValue2' }, function() {
+                    logger.log(expectedLevel, expectedMsg, { propName1: 'propValue1', propName2: 'propValue2' }, function(error) {
+                        expect(error).to.be.null;
+
                         var query = new azure.TableQuery()
                                              .where('PartitionKey eq ?', expectedPartitionKey);
 
@@ -87,18 +89,72 @@ describe('azure logger', function() {
                             var actualLevel = result.entries[0].level._;
                             var actualMsg = result.entries[0].msg._;
                             var actualCreatedDatetime = result.entries[0].createdDateTime._;
+                            var actualPropName1 = result.entries[0].propName1._;
+                            var actualPropName2 = result.entries[0].propName2._;
 
                             expect(actualPartitionKey).to.be.equal(expectedPartitionKey);
                             expect(actualHostname).to.not.be.empty;
                             expect(actualPid).to.not.be.empty;
-                            expect(actualLevel).to.not.be.empty;
+                            expect(actualLevel).to.be.equal(expectedLevel);
                             expect(actualMsg).to.be.equal(expectedMsg);
-                            expect(actualMsg).to.not.be.empty;
+                            expect(actualPropName1).to.be.equal('propValue1');
+                            expect(actualPropName2).to.be.equal('propValue2');
                             done();
                         });
                     });
                 }
             });
-       });
+        });
+    });
+
+    describe('query', function() {
+        var logger;
+
+        beforeEach(function(done) {
+            logger = new winston.transports.AzureLogger({
+                useDevStorage: true,
+                callback: function() {
+                    done();
+                }
+            });
+        });
+
+        it('happy path', function(done) {
+            var expectedLevel = Math.random().toString(36).replace(/[^a-z]+/g, '');
+            var expectedMsg = Math.random().toString(36).replace(/[^a-z]+/g, '');
+
+            logger.log(expectedLevel, expectedMsg, function(error) {
+                expect(error).to.be.null;
+
+                logger.query(null, function(error, result) {
+                    expect(error).to.be.null;
+                    expect(result).to.have.length('1');
+                    expect(result[0].level).to.be.equal(expectedLevel);
+                    expect(result[0].msg).to.be.equal(expectedMsg);
+                    done();
+                });
+            });
+        });
+
+        it('expectedFields', function(done) {
+            var expectedMsg = Math.random().toString(36).replace(/[^a-z]+/g, '');
+
+            logger.log(null, expectedMsg, function(error) {
+                expect(error).to.be.null;
+
+                var options = {
+                    fields: ['msg']
+                };
+
+                logger.query(options, function(error, result) {
+                    expect(error).to.be.null;
+                    expect(result).to.have.length('1');
+                    expect(Object.keys(result[0])).to.have.length('1');
+                    expect(result[0].msg).to.be.equal(expectedMsg);
+                    done();
+                });
+            });
+        });
+
     });
 });
